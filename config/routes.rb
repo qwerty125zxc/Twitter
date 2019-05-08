938bb0813597a5ac9a6033e3f718c9d4abb82b06
VasyaKozar <vasyakozar13@gmail.com>
2019-05-08 11:36:52 +0200

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'profiles/new'
+  get 'profiles/edit'
+  get 'profiles/show'
+  get 'posts/new'
+  get 'posts/show'
+  get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
