761f721de34d4691d01bd14294158cc07730e4c9
VasyaKozar <vasyakozar13@gmail.com>
2019-05-08 11:53:04 +0200

    add model

---
+++ app/models/userprofile.rb
@@ -0,0 +1,2 @@
+class Userprofile < ApplicationRecord
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'profiles/new'
   get 'profiles/edit'
   get 'profiles/show'
   get 'posts/new'
   get 'posts/show'
-  get 'pages/index'
+  root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
