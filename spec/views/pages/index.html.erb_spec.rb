cd42a192ec987fb54c315279b7bf0e7c8b48af47
r_afanasyev <romaaf2000@gmail.com>
2019-05-15 00:32:01 +0300

    tests for views

--- spec/views/pages/index.html.erb_spec.rb
+++ spec/views/pages/index.html.erb_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "pages/index", type: :view do
   let!(:user){create(:user)}
-  let!(:profile){create(:profile, user: user)}
+  let!(:profile){create(:userprofile, user: user)}
 
 
   before :each do
-    @posts = assign(:post, [Post.create!(title: "title1", body: "body1", user: user),
-      Post.create!(title: "title2", body: "body2", user: user)])
+    @posts = assign(:post, [Post.create!(title: "title1", body: "body1", user: user, like: 0),
+      Post.create!(title: "title2", body: "body2", user: user, like: 0)])
     controller.instance_variable_set(:@posts, @posts)
-    allow(view).to receive_messages(:will_paginate => nil)
   end
 
     it "should show a post title"   do
@@ -18,10 +17,10 @@ RSpec.describe "pages/index", type: :view do
       expect(rendered).to include 'title1'
     end
 
-    it "should show a link to post title" do
+    it "should show a link to post" do
       render
-      expect(rendered).to include '<a href="/posts/1">title1</a>'
-      expect(rendered).to include '<a href="/posts/2">title2</a>'
+      expect(rendered).to include '<a href="/posts/1">Read More</a>'
+      expect(rendered).to include '<a href="/posts/2">Read More</a>'
     end
 
     it "should show a post body" do
@@ -32,17 +31,12 @@ RSpec.describe "pages/index", type: :view do
 
     it "should show a link to new post" do
       render
-      expect(rendered).to include 'a href="/posts/new">New Post</a>'
+      expect(rendered).to include '<a href="/posts/new">Create new post</a>'
     end
 
-    it "should show a user email" do
+    it "should show a user name" do
       render
-      expect(rendered).to include 'tester@test.com'
+      expect(rendered).to include 'User <b>tester</b>  write: '
     end
 
-    it "should show a user avatar" do
-      render
-      expect(@posts[0].user.profile.avatar_url).to eq('/avatar/default.jpeg')
-      expect(rendered).to include '<img class="rounded-circle" src="/avatar/default.jpeg" alt="Default" width="20" height="20" />'
-    end
   end
