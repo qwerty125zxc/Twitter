6547093a05a5d8e4e52a41c30eb481dd0290f161
VasyaKozar <vasyakozar13@gmail.com>
2019-05-15 11:05:49 +0200

    shonebu

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def index
+  	if user_signed_in?
+  		if current.user.userprofile.nil?
+  			redirect_to new_userprofile_paths
+  		end
+  	end
   	@posts = Post.all
   end
 end
